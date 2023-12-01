9c8d2b10ba60f21567ab79e2f8018c2ea2c56bc3
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-01 10:39:09 -0700

    Forgot to add these files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,19 @@ export let data;
 </script>
 
 <!-- These are all the columns you can use, will populate more later -->
-{#each data.blogs as blog}
+<!-- {#each data.blogs as blog}
 		<div class="blog">
-			<!-- Title of blog -->
+			Title of blog
 				<h2>{blog.title}</h2>
-				<!-- Short summary, not sure if you want to use this much or not. Good for cards and when clicked, shows blogpost -->
+				Short summary, not sure if you want to use this much or not. Good for cards and when clicked, shows blogpost
 				<p>{blog.summary}</p>
-				<!-- Full blog post -->
+				Full blog post
 				<p>{blog.blogpost}</p>
-				<!-- Categories, We have Tech, Ai, Development, Software Engineering and Web Development  -->
+				Categories, We have Tech, Ai, Development, Software Engineering and Web Development 
 				<p>{blog.category}</p>
-				<!-- Self explanatory -->
+				Self explanatory
 				<p>{blog.datecreated}</p>
 		</div>
-	{/each}
+	{/each} -->
 
 <!-- Dynamic Routing test -->
