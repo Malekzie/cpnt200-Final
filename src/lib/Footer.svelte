e4dea1b5dd20504843356dd00c110249b47bdfd4
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-01 11:23:24 -0700

    animations for icons

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -15,15 +15,75 @@
   </div>
   
   <div class="flex gap-3 mt-[2em]">
-    <a href="/" target="_blank" rel="noopener noreferrer">
+    <a href="/" target="_blank" rel="noopener noreferrer" class="transact1">
       <Icon icon="ic:baseline-facebook" class="text-3xl"/>
     </a>
-    <a href="/" target="_blank" rel="noopener noreferrer">
+    <a href="/" target="_blank" rel="noopener noreferrer" class="transact2">
       <Icon icon="mdi:instagram" class="text-3xl"/>
     </a>
-    <a href="/" target="_blank" rel="noopener noreferrer">
+    <a href="/" target="_blank" rel="noopener noreferrer" class="transact3">
       <Icon icon="pajamas:twitter" class="text-3xl"/>
     </a>
   </div>
 </footer>
+
+
+<style>
+  .transact1{
+    animation: myAnim 2s ease-in-out 0s infinite reverse backwards;
+  }
+
+  .transact2{
+    animation: myAnim 2.5s ease-in-out 0s infinite reverse backwards;
+  }
+  .transact3{
+    animation: myAnim 3s ease-in-out 0s infinite reverse backwards;
+  }
+ 
+  @keyframes myAnim {
+	0% {
+		transform: translate(0);
+	}
+
+	10% {
+		transform: translate(-2px, -2px);
+	}
+
+	20% {
+		transform: translate(2px, -2px);
+	}
+
+	30% {
+		transform: translate(-2px, 2px);
+	}
+
+	40% {
+		transform: translate(2px, 2px);
+	}
+
+	50% {
+		transform: translate(-2px, -2px);
+	}
+
+	60% {
+		transform: translate(2px, -2px);
+	}
+
+	70% {
+		transform: translate(-2px, 2px);
+	}
+
+	80% {
+		transform: translate(-2px, -2px);
+	}
+
+	90% {
+		transform: translate(2px, -2px);
+	}
+
+	100% {
+		transform: translate(0);
+	}
+}
+</style>
   
