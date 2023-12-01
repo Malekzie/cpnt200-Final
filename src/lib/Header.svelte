092426de87e0a5bcde422783a29407156f2cdcf8
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-01 14:18:06 -0700

    Fixed merge conflict

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-<header class="bg-gray-800 p-4 flex items-center justify-between">
-  <strong class="text-2xl font-extrabold text-white">AI Saga</strong>
-  <strong class="border border-white px-4 py-2 text-white">Open AI News</strong>
-  
-  <nav class="flex items-center space-x-6">
-      <a class="text-white hover:text-secondary-200" href="/">Home</a>
-      <a class="text-white hover:text-secondary-200" href="/tab1">Tab 1</a>
-      <a class="text-white hover:text-secondary-200" href="/tab2">Tab 2</a>
-  </nav>
-  
-</header>
-=======
+
 <section class="flex justify-between border border-solid bg-slate-700">
     <div class="p-5 border-r border-solid">
       <h1 class="text-5xl">AIS</h1>
@@ -20,14 +8,13 @@
     </div>
     <div class="p-5 border-l border-solid">
       <ul class="flex gap-5 mt-3 text-xl">
-        <a href="/" class="transition delay-150 hover:scale-110 hover:text-slate-400">
+        <a href="/" class="transition delay-75 hover:scale-110 hover:text-slate-400">
           <li>Home</li>
-        </a><a href="/blogs" class="transition delay-150 hover:scale-110 hover:text-slate-400">
+        </a><a href="/blogs" class="transition delay-75 hover:scale-110 hover:text-slate-400">
           <li>Blogs</li>
-        </a><a href="contact" class="transition delay-150 hover:scale-110 hover:text-slate-400">
+        </a><a href="contact" class="transition delay-75 hover:scale-110 hover:text-slate-400">
           <li>Contact</li>
         </a>
       </ul>
     </div>
 </section>
->>>>>>> 58618a00f9211b2d149d98f52cf40829606a04b7
