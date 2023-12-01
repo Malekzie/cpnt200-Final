8a3029bc413b6753bb3d823fd343581ce9387c9c
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-01 10:37:12 -0700

    Made Footer component

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,21 @@
+<footer>
+  <div class="company-info">
+    <h3>Company Name</h3>
+    <p>123 Main Street, City, State, ZIP</p>
+    <p>Phone: 123-456-7890</p>
+    <p>Email: info@example.com</p>
+  </div>
+  
+  <div class="social-media">
+    <a href="/" target="_blank" rel="noopener noreferrer">
+      <img src="facebook-icon.png" alt="Facebook">
+    </a>
+    <a href="/" target="_blank" rel="noopener noreferrer">
+      <img src="twitter-icon.png" alt="Twitter">
+    </a>
+    <a href="/" target="_blank" rel="noopener noreferrer">
+      <img src="instagram-icon.png" alt="Instagram">
+    </a>
+  </div>
+</footer>
+  
