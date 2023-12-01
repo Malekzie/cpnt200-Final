140f8296f68141b88bef5d1c7e5fc3f1b5f54fa8
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-01 13:33:43 -0700

    Small fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,19 @@
 <script>
   // Module Imports
-  import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+  import { AppShell} from '@skeletonlabs/skeleton';
   import '../app.postcss';
 
   // Components
   import Footer from '$lib/Footer.svelte';
+	import Header from '$lib/Header.svelte';
 </script>
 
 <AppShell>
   <!-- Header -->
   <svelte:fragment slot="header">
-    <AppBar background="bg-primary-800">
-      <svelte:fragment slot="lead">
-        <strong class="text-2xl font-extrabold text-white">Website</strong>
-      </svelte:fragment>
-      <svelte:fragment slot="trail">
-        <a class="mx-2 text-white transition duration-300 ease-in-out hover:text-secondary-200 h3" href="/">Home</a>
-        <a class="mx-2 text-white transition duration-300 ease-in-out hover:text-secondary-200 h3" href="/tab">Tab</a>
-        <a class="mx-2 text-white transition duration-300 ease-in-out hover:text-secondary-200 h3" href="/tab">Tab</a>
-      </svelte:fragment>
-    </AppBar>
+  <Header />
   </svelte:fragment>
-<<<<<<< HEAD
-  
-=======
 
-  <a href="/">Home</a>
-  <a href="/pageone">One</a>
-  <a href="/pagetwo">Two</a>
-  <a href="/pagethree">Three</a>
->>>>>>> e7d013ed1787165536113ef1b397c2d50be2f2c1
   <slot />
 
   <!-- Footer -->
--- src/routes/[blog]/+page.js
+++ src/routes/[blog]/+page.js
@@ -1,4 +1,6 @@
 /** @type {import('./$types').PageLoad} */
-export async function load() {
-        throw error(404, {message: 'Page not found'});
-};
+// export async function load() {
+//     if (error) { 
+//         throw error(404, {message: 'Page not found'});
+//     }
+// };
