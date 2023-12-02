b87616f9b672f6b16cdde46fe070e156e4bcccb4
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-01 21:03:11 -0700

    linked cta to blogs

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -7,9 +7,11 @@
     <p class="m-4 text-2xl leading-relaxed font-lato">
       Step into the dynamic world of artificial intelligence with our daily blog, dedicated to unraveling the wonders of OpenAI. At AI Saga, we bring you a daily dose of engaging content, exploring the latest breakthroughs, applications, and stories behind the scenes of OpenAI's cutting-edge research.
     </p>
-    <button class="px-4 py-2 m-4 text-lg bg-red-700 hover:bg-red-800 active:bg-red-900">
-      Learn More
-    </button>
+    <a href="/blogs">
+      <button class="px-4 py-2 m-4 text-lg bg-red-700 hover:bg-red-800 active:bg-red-900">
+        Learn More
+      </button>
+    </a>
   </div>
   <div class="w-max">
     <img src="/images/hero-image.jpg" alt="robot" class="w-full h-[49rem] rounded-2xl">
