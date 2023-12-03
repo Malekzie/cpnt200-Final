2e2443d96f84494a818d431750a234c75b8fb5bc
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-03 00:08:45 -0700

    Added new components

---
+++ src/lib/components/blog-components/BlogHeader.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<section>
+<div class="">
+  <h1>Blog Header</h1>
+</div>
+</section>
---
+++ src/lib/components/blog-components/SideBar.svelte
@@ -0,0 +1,3 @@
+<section class="w-[15rem] border-r border-b h-screen border-solid">
+
+</section>
