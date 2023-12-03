cc7598e28b774bca843224f7a03f50682fa3461e
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-03 00:07:47 -0700

    Changed Links

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,20 @@
 
 <section class="flex justify-between border border-solid bg-slate-700">
     <div class="p-5 border-r border-solid">
-      <h1 class="text-5xl">AIS</h1>
+      <h1 class="text-5xl"><a href="/">AIS</a></h1>
     </div>
     <div class="p-5">
-      <h1 class="text-4xl">AI Saga</h1>
+      <h1 class="text-4xl cursor-default">AI Saga</h1>
     </div>
     <div class="p-5 border-l border-solid">
       <ul class="flex gap-5 mt-3 text-xl">
-        <a href="/" class="transition delay-75 hover:scale-110 hover:text-slate-400">
-          <li>Home</li>
-        </a><a href="/blogs" class="transition delay-75 hover:scale-110 hover:text-slate-400">
+        <a href="/blog" class="transition delay-50 hover:scale-110 hover:text-slate-400">
           <li>Blogs</li>
-        </a><a href="contact" class="transition delay-75 hover:scale-110 hover:text-slate-400">
+        </a>
+        <a href="/create" class="transition delay-50 hover:scale-110 hover:text-slate-400">
+          <li>Create Blog</li>
+        </a>
+        <a href="contact" class="transition delay-50 hover:scale-110 hover:text-slate-400">
           <li>Contact</li>
         </a>
       </ul>
