81433369e4ece5b1be19a60e4e85e707ea1bc2ac
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-03 17:15:59 -0700

    Removed uneeded pages

--- src/routes/[blog]/+page.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-// export async function load() {
-//     if (error) { 
-//         throw error(404, {message: 'Page not found'});
-//     }
-// };
--- src/routes/[blog]/+page.svelte
+++
@@ -1,3 +0,0 @@
-<section class="h-screen">
-
-</section>
