ecd07b1fd035cb58488500b05a56b5e938601e6e
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-03 17:15:43 -0700

    Sidebar has links leading elsewhere
    Not yet working, still working on it

--- src/lib/components/blog-components/SideBar.svelte
+++ src/lib/components/blog-components/SideBar.svelte
@@ -1,3 +1,17 @@
-<section class="w-[15rem] border-r border-b h-screen border-solid">
+<script>
 
+</script>
+
+<section class="w-[18.8rem] border-l-2 border-b-2 h-full border-solid">
+<div>
+  <ul>
+    <a href="/blog/One">
+      <li>One</li>
+    </a><a href="/blog/Two">
+      <li>Two</li>
+    </a><a href="/blog/Three">
+      <li>Three</li>
+    </a>
+  </ul>
+</div>
 </section>
