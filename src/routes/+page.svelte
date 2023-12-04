453df0587257a059dcbd092cfe3cae8ed0121bdd
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-03 23:35:17 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,8 @@ import BlogCard from '../lib/components/BlogCard.svelte';
 <section class="h-screen mt-[10rem]">
 	<h1 class="text-5xl text-center">Blogs</h1>
 	<div class="flex flex-wrap justify-around">
-		{#each data.blogs as blog}
-			<BlogCard title={blog.title} author={blog.author} date={blog.date} summary={blog.summary}/>
-		{/each}
+			<BlogCard title={data.blogs[0].title} author={data.blogs[0].author} date={data.blogs[0].date} summary={data.blogs[0].summary} link="{data.blogs[0].slug}"/>
+			<BlogCard title={data.blogs[1].title} author={data.blogs[1].author} date={data.blogs[1].date} summary={data.blogs[1].summary} link="{data.blogs[1].slug}"/>
 	</div>
 </section>
 
