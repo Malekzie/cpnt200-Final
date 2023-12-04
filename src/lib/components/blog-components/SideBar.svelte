710e2de695c0600639dc1cec9b15c9e0a1f41153
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-03 23:28:35 -0700

    changed links

--- src/lib/components/blog-components/SideBar.svelte
+++ src/lib/components/blog-components/SideBar.svelte
@@ -5,11 +5,11 @@
 <section class="w-[18.8rem] border-l-2 border-b-2 h-full border-solid">
 <div>
   <ul>
-    <a href="/blog/One">
+    <a href="/one">
       <li>One</li>
-    </a><a href="/blog/Two">
+    </a><a href="/two">
       <li>Two</li>
-    </a><a href="/blog/Three">
+    </a><a href="/three">
       <li>Three</li>
     </a>
   </ul>
