34a1bbbc58634230b7ad57281c97f450fa4f6138
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-03 17:15:22 -0700

    Added header content

--- src/lib/components/blog-components/BlogHeader.svelte
+++ src/lib/components/blog-components/BlogHeader.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <section>
-<div class="">
-  <h1>Blog Header</h1>
+<div class="w-full mx-auto text-center border-b-2 border-solid bg-slate-700">
+  <h1 class="p-3 text-5xl">AI Saga</h1> 
+    <p class="m-2">Where the latest news about AI is found</p>
 </div>
 </section>
