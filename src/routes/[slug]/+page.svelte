3ecae7428b1fe76ae25bbfde9b106f4b562a9aa6
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-04 00:24:31 -0700

    Styled slug information

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,21 +2,19 @@
 	export let data;
 </script>
 
-<section class="relative h-screen">
-	{#each data.blogs as blog}
-		<h1 class="text-5xl text-center">{blog.title}</h1>
-		<div class="flex justify-between">
-            <p>{blog.author}</p>
-            <div class="flex gap-5 m-5">
-                {#each blog.category as tags}
-                    <span>{tags}</span>
+<section class="h-full m-5 rounded-lg bg-slate-700">
+    <div class="p-5 mx-5 bg-slate-800 rounded-br-xl rounded-bl-xl">
+        <h1 class="text-5xl text-center">{data.title}</h1>
+        <div class="flex justify-around mt-2">
+            <p class="text-2xl">Author: <span class="font-bold">{data.author}</span></p>
+            <div class="flex w-[30rem] flex-wrap gap-5">
+                {#each data.category as tags}
+                <span class="text-md chip variant-glass-surface">{tags}</span>
                 {/each}
             </div>
         </div>
-		<div>
-			{@html blog.blogpost}
-		</div>
-	{/each}
-
-    {data.blogs[0].title}
+    </div>
+    <div class="p-5 m-20 text-xl rounded-lg bg-slate-800">
+        <p>{@html data.blogpost}</p>
+    </div>
 </section>
